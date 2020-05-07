4e9b52a1f25066f8bedb3d4c206883a472d9c7f7
Bruno Neckel <neckel.bw@gmail.com>
2020-05-07 14:32:50 -0300

    DeliverymanController changes

--- back-end/src/app/controllers/DeliverymanController.js
+++ back-end/src/app/controllers/DeliverymanController.js
@@ -1,13 +1,14 @@
 import * as Yup from 'yup';
-import Deliveryman from '../models/Deliverymans';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
 
 class DeliveryManController {
   async index(req, res) {
-    const { id, name, avatar_id, email } = Deliveryman.findAll({
-      attributes: ['id', 'name', 'email', 'avatar_id'],
+    const Deliverymans = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'avatar_id', 'email'],
     });
 
-    res.json({ id, name, avatar_id, email });
+    res.json(Deliverymans);
   }
 
   async store(req, res) {
@@ -26,7 +27,7 @@ class DeliveryManController {
     });
 
     if (deliverymanExist) {
-      return res.status(400).json({ error: 'Delivery Man already exists.' });
+      return res.status(400).json({ error: 'Delivery man already exists.' });
     }
 
     const { id, name, email } = await Deliveryman.create(req.body);
@@ -35,11 +36,77 @@ class DeliveryManController {
   }
 
   async update(req, res) {
-    res.json();
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      avatar_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    try {
+      const { name, email, avatar_id } = req.body;
+
+      const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+      if (email && email !== deliveryman.email) {
+        const emailExist = await Deliveryman.findOne({
+          where: { email: req.body.email },
+        });
+
+        if (emailExist) {
+          return res
+            .status(400)
+            .json({ error: 'This email already exist, try other.' });
+        }
+      }
+
+      if (email && email === deliveryman.email) {
+        return res.status(400).json({ error: 'Cannot use the same email.' });
+      }
+
+      if (name && name === deliveryman.name) {
+        return res
+          .status(400)
+          .json({ error: 'The name is the same as that registered.' });
+      }
+
+      if (avatar_id && avatar_id == deliveryman.avatar_id) {
+        return res
+          .status(400)
+          .json({ error: 'The image is the same as the one being used.' });
+      }
+
+      const fileExist = await File.findOne({ where: { id: avatar_id } });
+
+      if (!fileExist) {
+        return res
+          .status(400)
+          .json({ error: "The image does't exist in database." });
+      }
+
+      const { id } = await deliveryman.update(req.body);
+
+      return res.json({ id, name, email, avatar_id });
+    } catch (err) {
+      return res.status(400).json({ error: "Delivery man does't exist." });
+    }
   }
 
   async delete(req, res) {
-    res.json();
+    try {
+      const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+      await deliveryman.destroy();
+
+      return res.status(200).json({ message: 'Success in deleting.' });
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ erro: 'Delivery man not found or not exist.' });
+    }
   }
 }
 
